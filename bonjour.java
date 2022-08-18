439295af984ed1d2413b999cbe2ae0479fe34bb5
damtoti <richegod@gmail.com>
2022-08-18 12:58:33 +0200

    Update bonjour.java

--- bonjour.java
+++ bonjour.java
@@ -1,11 +1,6 @@
-// Définition d'une classe
-class HelloWorld 
-{ 
-    /* Votre programme commence par un appel à main(). 
-    Affiche "Hello, World" dans la fenêtre du terminal. */
- 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
-    } 
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("bonjour damtoti");
+    }
 }
